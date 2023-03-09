ac9c5e2461d49dce2adc6ae3249b26b55866466c
Marc LeBlanc <marc@leblanchq.com>
2023-03-09 08:59:18 -0500

    consul helm chart options
    variablized options for the consul helm chart + added pre-commit hooks and a GH action that can be used for status checks used in PRs

--- variables.tf
+++ variables.tf
@@ -1,9 +1,29 @@
 variable "consul_namespace" {
   description = "Namespace for consul deployment on GKE"
   default     = "consul"
+  type        = string
 }
 
 variable "consul_type" {
   description = "Insecure/Secure"
   default     = "secure"
+  type        = string
+}
+
+variable "consul_dc_name" {
+  description = "Consul Datacenter Name"
+  default     = "dc-gke"
+  type        = string
+}
+
+variable "federation_toggle" {
+  type        = string
+  description = "Toggle Consul Mesh federation on/off"
+  default     = "false"
+}
+
+variable "meshgateway_toggle" {
+  type        = string
+  description = "Toggle Consul Mesh gateway on/off"
+  default     = "false"
 }
